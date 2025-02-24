8c5b03327696927d275a160eb4ecfba5bdd31529
samiha-mahin <samihamahin36@gmail.com>
2025-02-25 01:12:34 +0600

    volunteer

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './utils/db.js';
+dotenv.config({});
 
 const app = express();
 
@@ -16,7 +19,8 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-const PORT = 3000;
-app.listen (PORT,()=>{
-   console.log(`Server Running at Port ${PORT}`);
-})
+const PORT = process.env.PORT || 3000;
+ app.listen (PORT,()=>{
+    connectDB();
+    console.log(`Server Running at Port ${PORT}`);
+ });
