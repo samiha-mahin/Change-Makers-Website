e9f712fc5647bb6ab1406486abb88aba360ee8c6
samiha-mahin <samihamahin36@gmail.com>
2025-02-25 23:31:45 +0600

    volunteer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
 
 const App = () => {
   return (
-    <div>
-      <h1 className='text-4xl text-blue-800 font-bold'>Lets build volunteer portal</h1>
-    </div>
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ frontend/src/components/shared/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from '../ui/button'
+
+
+const Navbar = () => {
+  return (
+    <div>
+        <h1 className='text-blue-600'>Navbar</h1>
+        <Button>Click</Button>
+    </div>
+  )
+}
+
+export default Navbar
