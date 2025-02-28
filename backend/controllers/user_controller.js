dfceeec7d3d45bf58e84f955db97ca80756e8555
samiha-mahin <samihamahin36@gmail.com>
2025-03-01 00:50:40 +0600

    v

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -5,14 +5,15 @@ import jwt from 'jsonwebtoken';
 export const register = async (req,res) => {
     try {
         const {fullname, email, phoneNumber, password, role} = req.body;
-
         if(!fullname || !email || !phoneNumber || !password || !role){
             return res.status(400).json({
             message: "All fields are required",
             success: false,
             });
         }
+
         //cloudinary upload
+
         const user = await User.findOne({email});
         if (user) {
             return res.status(400).json({
@@ -20,7 +21,9 @@ export const register = async (req,res) => {
               success: false,
             });
           }
+          
         const hashedPassword = await bcrypt.hash(password, 10);
+
         await User.create({
             fullname,
             email,
@@ -31,6 +34,7 @@ export const register = async (req,res) => {
             //   profilePhoto: cloudResponse.secure_url ,
             // }
           });
+
         return res.status(201).json({
             message: "Account created successfully",
             success: true,
