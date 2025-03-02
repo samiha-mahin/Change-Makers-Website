864af1e55570c2cc7297cd671a09257480d7a7f0
samiha-mahin <samihamahin36@gmail.com>
2025-03-02 16:09:53 +0600

    v

---
+++ backend/routes/user_route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import {login, logout, register, updateProfile} from '../controllers/user_controller.js';
+import isAuthenticated from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").get(logout);
+router.route("/profile/update").post(isAuthenticated, updateProfile);
+
+export default router;
