19f5fd1d7080f38505fe96ebc3228ab8fb6e7278
samiha-mahin <samihamahin36@gmail.com>
2025-03-02 14:16:09 +0600

    v

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -1,47 +1,113 @@
-import {User} from '../models/user_model.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import { User } from "../models/user_model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-export const register = async (req,res) => {
-    try {
-        const {fullname, email, phoneNumber, password, role} = req.body;
-        if(!fullname || !email || !phoneNumber || !password || !role){
-            return res.status(400).json({
-            message: "All fields are required",
-            success: false,
-            });
-        }
-
-        //cloudinary upload
-
-        const user = await User.findOne({email});
-        if (user) {
-            return res.status(400).json({
-              message: "User already exists with this email",
-              success: false,
-            });
-          }
-          
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        await User.create({
-            fullname,
-            email,
-            phoneNumber,
-            password: hashedPassword,
-            role,
-            // profile:{
-            //   profilePhoto: cloudResponse.secure_url ,
-            // }
-          });
-
-        return res.status(201).json({
-            message: "Account created successfully",
-            success: true,
-          });
-    } catch (error) {
-        return res.status(500).json({
-            message: "Registration failed",
-          });
+export const register = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, password, role } = req.body;
+    if (!fullname || !email || !phoneNumber || !password || !role) {
+      return res.status(400).json({
+        message: "All fields are required",
+        success: false,
+      });
     }
-}
+
+    //cloudinary upload
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        message: "User already exists with this email",
+        success: false,
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      fullname,
+      email,
+      phoneNumber,
+      password: hashedPassword,
+      role,
+      // profile:{
+      //   profilePhoto: cloudResponse.secure_url ,
+      // }
+    });
+
+    return res.status(201).json({
+      message: "Account created successfully",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Registration failed",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password, role } = req.body;
+
+    if (!email || !password || !role) {
+      return res.status(400).json({
+        message: "All fields are required",
+        success: false,
+      });
+    }
+
+    let user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Incorrect email or password",
+        success: false,
+      });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      return res.status(400).json({
+        message: "Incorrect email or password",
+        success: false,
+      });
+    }
+    if (role !== user.role) {
+      return res.status(400).json({
+        message: "Account doesn't exist with the current role",
+        success: false,
+      });
+    }
+
+    const tokenData = {
+      userId: user._id,
+    };
+    const token = await jwt.sign(tokenData, process.env.SECRET_KEY, {
+      expiresIn: "7 days",
+    });
+    user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpsOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: `Welcome back ${user.fullname}`,
+        user,
+        success: true,
+      });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Login failed",
+    });
+  }
+};
