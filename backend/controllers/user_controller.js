e9281ce853334902a7a1d20c108ba71dc0d1a111
samiha-mahin <samihamahin36@gmail.com>
2025-03-02 15:47:46 +0600

    v

--- backend/controllers/user_controller.js
+++ backend/controllers/user_controller.js
@@ -113,14 +113,62 @@ export const login = async (req, res) => {
 };
 
 export const logout = async (req, res) => {
-    try {
-        return res.status(200).cookie("token", "", {maxAge: 0}).json({
-            message: "Logout successful",
-            success: true
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: "Logout failed"
-        })
+  try {
+    return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+      message: "Logout successful",
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Logout failed",
+    });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const { fullname, email, phoneNumber, bio, skills } = req.body;
+
+    let skillsArray;
+    if (skills) {
+      skillsArray = skills.split(",");
     }
+
+    const userId = req.id; //middleware authentication
+    let user = await User.findById(userId);
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+        success: false,
+      });
+    }
+
+    //updating user
+
+    if (fullname) user.fullname = fullname;
+    if (email) user.email = email;
+    if (phoneNumber) user.phoneNumber = phoneNumber;
+    if (bio) user.profile.bio = bio;
+    if (skills) user.profile.skills = skillsArray;
+
+    await user.save();
+
+    user = {
+      _id: user._id,
+      fullname: user.fullname,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+      role: user.role,
+      profile: user.profile,
+    };
+    return res.status(200).json({
+      message: "Profile updated successfully",
+      user,
+      success: true,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Profile update failed",
+    });
+  }
 };
