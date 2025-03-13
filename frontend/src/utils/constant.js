73f6d680452def91b7cb2a7922d39cd7166bbf97
samiha-mahin <samihamahin36@gmail.com>
2025-03-13 14:50:48 +0600

    volunteer

---
+++ frontend/src/utils/constant.js
@@ -0,0 +1,4 @@
+export const USER_API = "http://localhost:5000/api/v1/user";
+export const DUTY_API = "http://localhost:5000/api/v1/duty";
+export const ORGANIZATION_API = "http://localhost:5000/api/v1/organization";
+export const APPLICATION_API = "http://localhost:5000/api/v1/app";
