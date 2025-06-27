5c47520a5c163caac0a08b51d8ee1300645a449e
samiha-mahin <samihamahin36@gmail.com>
2025-06-28 01:44:38 +0600

    change-makers

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 const Footer = () => {
   return (
     <footer className="border-t border-t-gray-200 py-8">
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-8">
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <motion.h1
