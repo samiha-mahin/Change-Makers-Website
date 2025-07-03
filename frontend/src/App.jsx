5561e8a716a1d057b3ac5179c0dedac254a27688
samiha-mahin <samihamahin36@gmail.com>
2025-07-03 21:07:28 +0600

    change-makers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Home from './components/Home'
 import Login from './components/auth/Login'
 import Signup from './components/auth/Signup'
+import Organizations from './components/admin/Organizations'
 
 const App = () => {
   return (
@@ -13,6 +14,8 @@ const App = () => {
         <Route path='/' element={<Home/>} />
         <Route path='/login'element={<Login/>}/>
         <Route path='/signup'element={<Signup/>}/>
+        //admin routes
+        <Route path='/admin/organizations' element={<Organizations/>}/>
       </Routes>
       </BrowserRouter>
     </>
