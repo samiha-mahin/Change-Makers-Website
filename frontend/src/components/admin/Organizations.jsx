d77f17577b387bb0e43ae988ba7099e924f5f1d2
samiha-mahin <samihamahin36@gmail.com>
2025-07-06 19:09:55 +0600

    change-makers

--- frontend/src/components/admin/Organizations.jsx
+++ frontend/src/components/admin/Organizations.jsx
@@ -1,11 +1,40 @@
 import useGetAllOrganizations from '@/hooks/useGetAllOrganizations'
-import React from 'react'
+import { setSearchOrganizationByText } from '@/redux/organizationSlice';
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import Navbar from '../shared/Navbar';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import OrganizationsTable from './OrganizationsTable';
 
 const Organizations = () => {
   useGetAllOrganizations();
-  
+  const [input,setInput]= useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    dispatch(setSearchOrganizationByText(input));
+  },[input]);
+
   return (
-    <div>Admin will register oragnization to create job</div>
+    <div>
+      <Navbar/>
+      <div className='max-w-6xl mx-auto my-10'>
+        <div className='flex justify-between items-center my-5'>
+          <Input
+          className="w-fit"
+          placeholder='Search Organization'
+          onChange = {(e)=>setInput(e.target.value)}
+          />
+          <Button onClick={()=>navigate("/admin/organizations/create")} className="text-white bg-[#467057] hover:bg-[#2A4B37]">
+            Register Organization
+          </Button>
+        </div>
+        <OrganizationsTable/>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/admin/OrganizationsTable.jsx
+++ frontend/src/components/admin/OrganizationsTable.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
+import Organizations from './Organizations';
 
 const OrganizationsTable = () => {
+  const {organizations , searchOrganizationByText} = useSelector((store)=> store.organization);
+  const navigate = useNavigate();
+  const [filterOrganization,setFilterOrganization] = useState(organizations);
+
+  useEffect(()=>{
+    const filteredOrganization = organizations.length >= 0 && organizations.filter((organization)=>{
+      if(!searchOrganizationByText) {
+        return true;
+      };
+      return organization?.name?.toLowerCase().includes(searchOrganizationByText.toLowerCase())
+    })
+    setFilterOrganization(filteredOrganization);
+  },[organizations, searchOrganizationByText]);
+  
   return (
     <div>OrganizationsTable</div>
   )
