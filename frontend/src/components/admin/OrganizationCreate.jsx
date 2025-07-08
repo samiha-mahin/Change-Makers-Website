73dbe470bc06a08f96277cc23ab3316ed8b77b3d
samiha-mahin <samihamahin36@gmail.com>
2025-07-09 00:34:07 +0600

    c

--- frontend/src/components/admin/OrganizationCreate.jsx
+++ frontend/src/components/admin/OrganizationCreate.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom'
 
 const OrganizationCreate = () => {
+  const navigate = useNavigate();
+  const [organizationName, setOrganizationName] = useState();
+  const dispatch = useDispatch();
   return (
     <div>OrganizationCreate</div>
   )
