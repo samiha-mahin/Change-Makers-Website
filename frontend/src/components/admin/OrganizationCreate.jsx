88e2d16c9c51cf4a3514083b46cdfa4a5bbe2bb0
samiha-mahin <samihamahin36@gmail.com>
2025-08-05 21:19:18 +0600

    c

--- frontend/src/components/admin/OrganizationCreate.jsx
+++ frontend/src/components/admin/OrganizationCreate.jsx
@@ -1,13 +1,37 @@
+import { setSingleOrganization } from '@/redux/organizationSlice';
+import { ORGANIZATION_API } from '@/utils/constant';
+import axios from 'axios';
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom'
+import Navbar from '../shared/Navbar';
 
 const OrganizationCreate = () => {
   const navigate = useNavigate();
   const [organizationName, setOrganizationName] = useState();
   const dispatch = useDispatch();
+  const registerNewOrganization = async () => {
+    try {
+      const res = await axios.post(`${ORGANIZATION_API}/register`,{organizationName},{
+        headers: {
+            'Content-Type': 'application/json',
+          },
+          withCredentials: true,
+      });
+      if(res?.data?.success){
+        dispatch(setSingleOrganization(res.data.organization));
+      }
+      toast.success(res.data.message);
+      const organizationId = res?.data?.organization?._id;
+      navigate(`/admin/organizations/${organizationId}`);
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
-    <div>OrganizationCreate</div>
+    <div>
+      <Navbar/>
+    </div>
   )
 }
 
